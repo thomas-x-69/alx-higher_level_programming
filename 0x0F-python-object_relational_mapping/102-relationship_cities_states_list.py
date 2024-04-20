a3a8ed5d52b82f391f968b1d8c931e7b209a08e9
thomas-x-69 <thomas.ashraf975@gmail.com>
2024-04-20 22:59:07 +0200

    added ORM

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """
-script that lists all City objects
-from the database hbtn_0e_101_usa
+Lists all City objects from the database hbtn_0e_101_usa
 """
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import sys
 from relationship_state import Base, State
 from relationship_city import City
-from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "__main__":
-    e = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                    argv[2],
-                                                    argv[3])
-    engine = create_engine(e)
-    Base.metadata.create_all(engine)
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
-    s = Session()
-    cities = s.query(City).all()
-    for city in cities:
-        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
-    s.close()
+    session = Session()
+
+    st = session.query(State).join(City).order_by(City.id).all()
+
+    for state in st:
+        for city in state.cities:
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 """
-lists all cities from the database hbtn_0e_4_usa
+List all cities from a database
 """
-if __name__ == "__main__":
+import sys
+import MySQLdb
 
-    import MySQLdb
-    from sys import argv
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    conect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                             passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = conect.cursor()
-    cursor.execute("""SELECT cities.id, cities.name, states.name
-    FROM cities
-    LEFT JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id ASC""")
-    query_rows = cursor.fetchall()
-    for row in query_rows:
-        print(row)
-    cursor.close()
-    conect.close()
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
